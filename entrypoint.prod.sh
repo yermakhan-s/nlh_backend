5092dc216e5878d85139e1e44a9848db746a8a98
yermakhan.serikbayev <yermakhan.serikbayev@nu.edu.kz>
2024-04-16 21:11:51 +0500

    docker compose

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -12,3 +12,11 @@ then
 fi
 
 exec "$@"
+
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate --noinput
