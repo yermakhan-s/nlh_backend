272f4a913e6485eb7643d71054b1e39b4ce9c1db
yermakhan.serikbayev <yermakhan.serikbayev@nu.edu.kz>
2024-04-16 20:18:56 +0500

    -hacknu

--- nlh_backend/settings.py
+++ nlh_backend/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'authentication',
-    'hacknu'
 
 ]
 
--- nlh_backend/urls.py
+++ nlh_backend/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('authentication.urls')),
-    path('api/', include('hacknu.urls')),
+    path('api/', include('authentication.urls'))
 ]
