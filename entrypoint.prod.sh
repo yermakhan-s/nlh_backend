e444adccd7128dcb917742374475ec6ae4bae2dd
yermakhan.serikbayev <yermakhan.serikbayev@nu.edu.kz>
2024-04-18 01:15:57 +0500

    fix

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -13,10 +13,3 @@ fi
 
 exec "$@"
 
-# Collect static files
-echo "Collect static files"
-python manage.py collectstatic --noinput
-
-# Apply database migrations
-echo "Apply database migrations"
-python manage.py migrate --noinput
