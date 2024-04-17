11764655d69d8bc8d1228b2e8e8e67c06eee8271
Yeldar <57894795+issa012@users.noreply.github.com>
2024-04-17 17:02:02 +0500

    feat: admin page shows user info

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("id", "username", "email", "is_staff", "is_active", )
+    list_filter = ("username", "email", "is_staff", "is_active",)
+    fieldsets = (
+        (None, {"fields": ("email", "username",
+                           "password")}),
+        ("Permissions", {"fields": ("is_staff",
+         "is_active", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "username", "password1", "password2", "is_staff",
+                "is_active", "groups", "user_permissions"
+            )}
+         ),
+    )
+    search_fields = ("email", "username")
+    ordering = ("email", "username")
