0758fae7ce9b15f022d75bbcf5f3bb0e1dc6ee84
yermakhan.serikbayev <yermakhan.serikbayev@nu.edu.kz>
2024-04-18 20:26:01 +0500

    jobs

--- vacancies/admin.py
+++ vacancies/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import VacancyCategory, Vacancy
+
+
+
+class VacancyInline(admin.TabularInline):
+    model = Vacancy
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(VacancyCategory)
+class VacancyCategoryAdmin(admin.ModelAdmin):
+    inlines = [VacancyInline]
--- vacancies/models.py
+++ vacancies/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+from authentication.models import User
+
 # Create your models here.
+
+class VacancyCategory(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+
+class Vacancy(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='vacancies')
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(VacancyCategory, on_delete=models.CASCADE, null=True, blank=True, related_name='vacancies')
+    image_url = models.FileField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    def __str__(self):
+        return self.name
