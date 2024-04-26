d22bda2f891efc6765cf9e6c284465dbb237e1ef
Yermakhan <yermakhan.serikbayev@nu.edu.kz>
2024-04-27 00:41:53 +0500

    date filter fix

--- events/views.py
+++ events/views.py
@@ -8,25 +8,15 @@ from rest_framework.decorators import action
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from authentication.authentication import JWTAuthentication
+from datetime import datetime
 
 class EventViewSet(ModelViewSet):
     serializer_class = EventSerializer
     queryset = Event.objects.all()
     authentication_classes = [JWTAuthentication]
     filter_backends = [filters.SearchFilter, DjangoFilterBackend]
-    filterset_fields=['category_id', 'club_id', 'date']
+    filterset_fields=['category_id', 'club_id', 'date__date']
     search_fields = ['name', 'description']
-
-    def get_queryset(self):
-        """
-        Optionally restricts the returned purchases to a given user,
-        by filtering against a `username` query parameter in the URL.
-        """
-        queryset = Event.objects.all()
-        date = self.request.query_params.get('date')
-        if date is not None:
-            queryset = queryset.filter(date__date=date)
-        return queryset
 
     @action(methods=['get'], detail=False)
     def get_event_categories(self, request, *args, **kwargs):
